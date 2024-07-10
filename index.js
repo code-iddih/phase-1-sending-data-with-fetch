acb59c3dcbfbc197ca56acef3e2d7b97001a3db6
code-iddih <idinyamwaya@gmail.com>
2024-07-10 12:57:48 +0300

    Adds POST Request

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your code here
+function submitData(name, email) {
+  let formData = {
+    name: name,
+    email: email
+  };
+
+    // Test 1 - Send Data
+  
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+  return fetch("http://localhost:3000/users", configObj)
+    
+  // Test 2 - Handle the Response
+
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      let newId = object.id;
+      document.body.innerHTML = newId;
+    })
+
+    // Test 3 - Handle Errors
+
+    .catch(function(error) {
+      document.body.innerHTML = error.message;
+    });
+}
+
